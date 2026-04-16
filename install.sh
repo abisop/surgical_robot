82d529f10ad3439c80be663a039ef787e6492ab7
Abhishek Choithani <abhishekchoithani24@gmail.com>
2026-04-16 12:34:22 +0530

    Add installation script for PX4 Autopilot

---
+++ install.sh
@@ -0,0 +1,7 @@
+
+sudo apt update 
+sudo apt install git 
+
+git clone https://github.com/PX4/PX4-Autopilot.git --recurse-submodules ~/drone/PX4-Autopilot
+
+source ~/drone/PX4-Autopilot/Tools/setup/ubuntu.sh
