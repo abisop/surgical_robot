f06fe7acf033bf0d8763019e867033da401a77a4
abisop <abhishekchoithani24@gmail.com>
2026-04-17 12:37:52 +0530

    preparing for isaac sim

--- install.sh
+++
@@ -1,7 +0,0 @@
-
-sudo apt update 
-sudo apt install git 
-
-git clone https://github.com/PX4/PX4-Autopilot.git --recurse-submodules ~/drone/PX4-Autopilot
-
-source ~/drone/PX4-Autopilot/Tools/setup/ubuntu.sh
